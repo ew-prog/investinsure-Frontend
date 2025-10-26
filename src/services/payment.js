cb58c3e7bbbcbfb94ec589d2078dc915da1ff719
ew-prog <ewanyama@live.co.uk>
2025-10-26 05:23:38 -0700

    Update payment.js

--- src/services/payment.js
+++ src/services/payment.js
@@ -1,16 +1,46 @@
 import axios from "axios";
 
-export const initiatePayment = async (agentId, leadCount, method) => {
-  try {
-    const res = await axios.post("http://localhost:5000/api/agents/purchase", {
-      agentId,
-      leadIds: [], // placeholder, select actual IDs
-      transactionId: "TXN_" + Date.now(),
-      method
-    });
-    return res.data;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+export const paystackInit = async (agentId, leadIds, token) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/payments/paystack/init",
+    { agentId, leadIds },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data.authorization_url;
+};
+
+export const paystackVerify = async (reference, agentId, leadIds, token) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/payments/paystack/verify",
+    { reference, agentId, leadIds },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data;
+};
+
+export const flutterwavePay = async (agentId, leadIds, token) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/payments/flutterwave",
+    { agentId, leadIds },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data.link;
+};
+
+export const mtnPay = async (agentId, leadIds, phone, token) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/payments/mtn",
+    { agentId, leadIds, phone },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data.message;
+};
+
+export const airtelPay = async (agentId, leadIds, phone, token) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/payments/airtel",
+    { agentId, leadIds, phone },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data.message;
 };
