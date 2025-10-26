0a4318b71bfc0fed62588d18d2ace92132e1d03b
ew-prog <ewanyama@live.co.uk>
2025-10-26 05:14:28 -0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-gray-100 text-gray-700 p-4 text-center">
+    &copy; {new Date().getFullYear()} InsvestInsure. All rights reserved.
+  </footer>
+);
+
+export default Footer;
